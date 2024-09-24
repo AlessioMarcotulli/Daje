3b2131effa80f758238d541a03e36f1846943c1a
daje <alceciola@gmail.com>
2024-09-24 16:31:14 +0200

    Primo tentativo

---
+++ prova2.py
@@ -0,0 +1,14 @@
+k = 3
+j = 5 
+print("Jay = ", j);
+
+# input relations !!
+nome = input("Inserisci nome: ")
+print("Ciao "+ nome)
+
+# esempio rettangolo
+a = float(input("inserisci altezza: "))
+b = float(input("inserisci base: "))
+
+area = a * b
+print("Area: ", area)
